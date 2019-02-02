a7ce419cfd45ba13e43a510accfa71af1cf8ecca
mkieboom <mkieboom@mapr.com>
2019-02-02 14:09:44 +0100

    Initial commit

--- launch.sh
+++ launch.sh
@@ -10,10 +10,17 @@ while true
 do
 
 # Get connected Wifi Clients into: /connected_devices.json
-curl -X POST -H "Content-Type: application/json" -d '{"queryType":"SQL", "query": "select t.datetime, t.mac as client_mac,t.sensor as sensor_name,t.signal as signal_strength from dfs.root.`/mapr-retail-demo/sensors-connecteddevices-db` t where t.datetime in (select t.datetime from dfs.root.`/mapr-retail-demo/sensors-connecteddevices-db` t group by t.datetime order by t.datetime desc limit 1)"}' http://$MAPR_IP:8047/query.json > /connected_devices.json
+curl -X POST -H "Content-Type: application/json" -d '{"queryType":"SQL", "query": "select t.datetime, t.mac as client_mac,t.sensor as sensor_name,t.signal as signal_strength from dfs.root.`/mapr-retail-demo/sensors-connecteddevices-db` t where t.datetime in (select t.datetime from dfs.root.`/mapr-retail-demo/sensors-connecteddevices-db` t group by t.datetime order by t.datetime desc limit 1) order by  t.signal ASC"}' http://$MAPR_IP:8047/query.json > /connected_devices.json
+
+# Convert connected wifi client list into html
+cat /connected_devices.json | jq -r '["Date Time,Client MAC,Signal strength"], (.rows[]|[.datetime,.client_mac,.signal_strength])|@tsv' > /connected_devices.html
+sed -i -e 's/,/<\/td><td>/g' /connected_devices.html
+sed -i -e 's/\t/<\/td><td>/g' /connected_devices.html
+sed -i -e 's/^/<tr><td>/' /connected_devices.html
+sed -i -e 's/$/<\/td><\/tr>/g' /connected_devices.html
 
 # html header with auto-refresh
-cat <<EOF > /usr/share/nginx/html/index.html
+cat <<EOF > /index.html
 <html>
   <head>
     <title>Cash Register - Shop Status</title>
@@ -25,17 +32,17 @@ cat <<EOF > /usr/share/nginx/html/index.html
 EOF
 
 # connected devices
-cat /connected_devices.json | jq -r '["<tr><th>Date Time</th><th>Client MAC</th><th>Signal strength</th></tr><tr><td>"], (.rows[]|[.datetime,.client_mac,.signal_strength])|@tsv' >> /usr/share/nginx/html/index.html
+cat /connected_devices.html >> /index.html
+#cat /connected_devices.json | jq -r '["<tr><th>Date Time</th><th>Client MAC</th><th>Signal strength</th></tr><tr><td>"], (.rows[]|[.datetime,.client_mac,.signal_strength])|@tsv' >> /usr/share/nginx/html/index.html
 
 # html footer
-cat <<EOF >> /usr/share/nginx/html/index.html
+cat <<EOF >> /index.html
       </tr>
     </table>
   </body>
 </html>
 EOF
 
-# Fix the html table layout
-sed -i -e 's/\t/<\/td><td>/g' /usr/share/nginx/html/index.html
+/bin/cp -rf /index.html /usr/share/nginx/html/index.html
 
 done
