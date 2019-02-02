f28d213fd78f535f5f764d82cf28d4bc257959ae
mkieboom <mkieboom@mapr.com>
2019-02-02 16:17:09 +0100

    Initial commit

--- launch.sh
+++ launch.sh
@@ -10,10 +10,10 @@ while true
 do
 
 # Get connected Wifi Clients into: /connected_devices.json
-curl -X POST -H "Content-Type: application/json" -d '{"queryType":"SQL", "query": "select t.datetime, t.mac as client_mac,t.sensor as sensor_name,t.signal as signal_strength from dfs.root.`/mapr-retail-demo/sensors-connecteddevices-db` t where t.datetime in (select t.datetime from dfs.root.`/mapr-retail-demo/sensors-connecteddevices-db` t group by t.datetime order by t.datetime desc limit 1) order by  t.signal ASC"}' http://$MAPR_IP:8047/query.json > /connected_devices.json
+curl -X POST -H "Content-Type: application/json" -d '{"queryType":"SQL", "query": "select t1.datetime,t1.mac as client_mac,t1.sensor as sensor_name, t1.signal as signal_strength,t2.name, t2.email from dfs.root.`/mapr-retail-demo/sensors-connecteddevices-db` t1 left OUTER JOIN dfs.root.`/mapr-retail-demo/qrscanned-devices-db` t2 on t1.mac = t2.mac where t1.datetime in (select t.datetime from dfs.root.`/mapr-retail-demo/sensors-connecteddevices-db` t group by t.datetime order by t.datetime desc limit 1 ) group by t1.datetime, t1.mac, t1.sensor, t1.signal, t2.name, t2.email order by  t1.signal ASC"}' http://$MAPR_IP:8047/query.json > /connected_devices.json
 
 # Convert connected wifi client list into html
-cat /connected_devices.json | jq -r '["Date Time,Client MAC,Signal strength"], (.rows[]|[.datetime,.client_mac,.signal_strength])|@tsv' > /connected_devices.html
+cat /connected_devices.json | jq -r '["Date Time,Client MAC,Signal strength,Name,Email"], (.rows[]|[.datetime,.client_mac,.signal_strength,.name,.email])|@tsv' > /connected_devices.html
 sed -i -e 's/,/<\/td><td>/g' /connected_devices.html
 sed -i -e 's/\t/<\/td><td>/g' /connected_devices.html
 sed -i -e 's/^/<tr><td>/' /connected_devices.html
